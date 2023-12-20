c93fc093e2ab5a12278065ec5d5dfd407b2adbff
CesarTufiño <cesar.tufino@epn.edu.ec>
2023-12-20 09:26:27 -0500

    Intents

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.b2023gr2sw
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.net.Uri
+import android.provider.ContactsContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +19,28 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener{
                 irActividad(ACicloVida::class.java)
             }
-
         val botonListView = findViewById<Button>(R.id.btn_ir_list_view)
         botonListView
             .setOnClickListener{
                 irActividad(BListView::class.java)
+            }
+        val botonIntentImplicito = findViewById<Button>(
+            R.id.btn_ir_intent_implicito)
+        botonIntentImplicito
+            .setOnClickListener{
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                callbackIntentPickUri.launch(intentConRespuesta)
+            }
+        val botonIntentExplicito = findViewById<Button>(
+            R.id.btn_ir_intent_explicito)
+        botonIntentExplicito
+            .setOnClickListener{
+                abrirActividadConParametros(
+                    CIntentExplicitoParametros::class.java
+                )
             }
     }
 
@@ -29,4 +50,66 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, clase)
         startActivity(intent)
     }
+
+    fun abrirActividadConParametros(
+        clase: Class<*>
+    ){
+        val intentExplicito = Intent(this, clase)
+        // Enviar parametros (solamente variables primitivas)
+        intentExplicito.putExtra("nombre","Adrian")
+        intentExplicito.putExtra("apellido","Eguez")
+        intentExplicito.putExtra("edad",34)
+
+        callbackContenidoIntentExplicito.launch(intentExplicito)
+    }
+
+    val callbackContenidoIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                if (result.data != null){
+                    // Lógica Negocio
+                    val data = result.data
+                    mostrarSnackbar(
+                        "${data?.getStringExtra("nombreModificado")}"
+                    )
+                }
+            }
+        }
+
+    fun mostrarSnackbar (texto:String){
+        Snackbar
+            .make(
+                findViewById(R.id.id_layout_main), // view
+                texto, // texto
+                Snackbar.LENGTH_LONG // tiempo
+            )
+            .show()
+    }
+
+    val callbackIntentPickUri =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+                result ->
+            if (result.resultCode == RESULT_OK){
+                if (result.data != null){
+                    if (result.data!!.data != null){
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(
+                            uri, null, null, null, null, null)
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefono!!)
+                        cursor?.close()
+                        mostrarSnackbar("Telefono ${telefono}")
+                    }
+                }
+            }
+        }
+
 }
