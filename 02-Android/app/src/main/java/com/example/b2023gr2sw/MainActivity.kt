c9dfc779f476bede8693ee5e659cf07588348a6e
CesarTufiño <cesar.tufino@epn.edu.ec>
2024-02-10 11:35:20 -0500

    Clase 25

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -86,6 +86,10 @@ class MainActivity : AppCompatActivity() {
         intentExplicito.putExtra("apellido","Eguez")
         intentExplicito.putExtra("edad",34)
 
+        intentExplicito.putExtra("entrenador",
+            BEntrenador(1,"Nombre","Descripcion")
+        )
+
         callbackContenidoIntentExplicito.launch(intentExplicito)
     }
 
